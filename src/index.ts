e11fb1a8673a3468ec50662721f671fa53d23ccc
vinhdothanh <vinh.dothanh912@gmail.com>
2023-05-19 19:43:27 +0700

    initial project & set up typescript

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server is running right");
+});
+
+app.listen(port, () => {
+  console.log(
+    `⚡️[server]: Server is running at http://localhost:${port} is running`
+  );
+});
